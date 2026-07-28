3b9ff385571c5a3978e2010a609086596d05d963
losprimosbienlocos-star <losprimosbienlocos@gmail.com>
2026-07-28 12:42:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const btnLluvia = document.getElementById("btnLluvia");
+
+const btnFotos = document.getElementById("btnFotos");
+
+btnLluvia.onclick = () => {
+
+    location.href = "lluvia.html";
+
+}
+
+btnFotos.onclick = () => {
+
+    location.href = "corazon.html";
+
+}
+document.getElementById("volver").onclick=()=>{
+
+location.href="index.html";
+
+}
